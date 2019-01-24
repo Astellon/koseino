3760222bcf66de25878910b5da17a477e07faf13
astellon <astellon2525otastepper@gmail.com>
2019-01-24 15:30:10 +0900

    add test

---
+++ spec/kosino_spec.cr
@@ -0,0 +1,28 @@
+require "spec"
+# avoid executing top level programm
+# require "../src/koseino.cr"
+
+# equiv
+require "../src/lexeme.cr"
+require "../src/parser.cr"
+require "../src/eval.cr"
+
+describe Koseino do
+  describe "evaluate" do
+    it "print(1+1+1+1)" do
+      io = IO::Memory.new
+      t = Koseino::Lexer.new().parse("print(1+1+1+1)").tokens
+      root, pos = Koseino::Parser.new().parse(t, 0)
+      Koseino::Evaluater.new(io).eval(root)
+      io.to_s.should eq "4\n"
+    end
+
+    it "print(10*2-(5+5)*2+10)" do
+      io = IO::Memory.new
+      t = Koseino::Lexer.new().parse("print(10*2-(5+5)*2+10)").tokens
+      root, pos = Koseino::Parser.new().parse(t, 0)
+      Koseino::Evaluater.new(io).eval(root)
+      io.to_s.should eq "10\n"
+    end
+  end
+end
